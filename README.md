f6bbf0f95d005da82ffed7b0b5066f2b0a7607e5
Alan <alan@projectmccoll.co.uk>
2014-01-03 16:37:54 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 magento-change-admin-theme
 ==========================
+This extension shows a way of changing the Magento admin theme. In this example, the theme is changed to: mytheme.
---
+++ app/code/local/PMC/Adminhtml/Block/Page.php
@@ -0,0 +1,11 @@
+<?php
+
+class PMC_Adminhtml_Block_Page extends Mage_Adminhtml_Block_Template
+{
+
+	public function __construct()
+    {
+        Mage::getDesign()->setTheme('mytheme');
+    }
+
+}
